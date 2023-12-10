da5290ede92acd4f3bd3d89b7ed458a42559119e
Mukund Kannan <72849055+HyperHacker23@users.noreply.github.com>
2023-12-10 17:52:17 +0530

    Add files via upload

--- rpsGame.py
+++ rpsGame.py
@@ -1,31 +1,43 @@
 import sys
 import random
 
+# Display the initial message
 print('ROCK, PAPER, SCISSORS')
+
+# Initialize win, loss, and draw counters
 win = 0
 loss = 0
 draw = 0
 
+# Main game loop
 while True:
+    # User input loop for choosing a move
     while True:
+        # Display current game statistics
         print('%s Wins, %s Losses, %s Ties' % (win, loss, draw))
         print('(r)ock, (p)aper, (s)cissors, (q)uit')
         print('Choose your move: ')
+        
         playerMove = str(input())
+        
+        # Check if the user wants to quit
         if playerMove == 'q':
             sys.exit()
+        # Check if the user entered a valid move
         elif playerMove == 'r' or playerMove == 'p' or playerMove == 's':
             break
         else:
             print('Please enter r, p, s or q')
-    #printing user's choice
+    
+    # Display user's chosen move
     if playerMove == 'r':
         print('Rock versus.....')
     elif playerMove == 'p':
         print('Paper versus.....')
     elif playerMove == 's':
         print('Scissors versus.....')
-    #choosing the move of computer
+    
+    # Generate computer's move
     computerMove = random.randint(1, 3)
     if computerMove == 1:
         computerMove = 'r'
@@ -36,7 +48,8 @@ while True:
     elif computerMove == 3:
         computerMove = 's'
         print('Scissors')
-    #determining who is the winner
+    
+    # Determine the winner and update statistics
     if computerMove == playerMove:
         print('Its a tie')
         draw = draw + 1
